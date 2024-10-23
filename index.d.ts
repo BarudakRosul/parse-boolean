98c1941d4d7f2b7c4ff14d7b7fd2aa432904dbd4
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2024-10-23 20:44:18 +0700

    fix: update declaration TypeScript file

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@
  * @param {boolean | string} value - The input value to parse as a boolean.
  * @returns {boolean | undefined} - The parsed boolean value, or undefined if parsing fails.
  */
-function parseBoolean(value: boolean | string): boolean | undefined;
+declare function parseBoolean(value: boolean | string): boolean | undefined;
 
 // Exports module
 export default parseBoolean;
